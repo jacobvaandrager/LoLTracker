8546a20552c0732426ecaf0503b5b0b523cb3669
Jacob Vaandrager <jacobvaandrager@icloud.com>
2020-06-03 12:12:33 +0200

    1

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,19 @@
-alert('Hello!');
+
+var player;
+
+function playerGet(){
+
+	player = document.getElementById("player").value;
+	setDetails();
+}
+
+
+function setDetails(){
+
+	document.getElementById('playerStats').innerHTML = player;
+
+}
+
+
+
+// check kda and see if they are good
